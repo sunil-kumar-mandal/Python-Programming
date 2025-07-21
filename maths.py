f4026c67d89707be14590d00d94080cc6bbc861b
Sunil Mandal <sunil.incode@gmail.com>
2025-07-21 05:20:38 +0000

    first math program

---
+++ maths.py
@@ -0,0 +1,15 @@
+print("calculator program")
+a=int(input("enter first no : "))
+b= int(input("enter second no : "))
+
+print("Calculation on " , a , " and " , b)
+
+print("Sum : " , a + b)
+
+print("subtraction : " , a-b)
+print("multiplication : " , a*b)
+print("division : " , a/b)
+
+print("Simple expression solution")
+exp = eval(input("Enter express : "))
+print("Result : ",exp)
