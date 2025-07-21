cd1a31ac70a7824144b763108c02d1032e9e6167
Sunil Mandal <sunil.incode@gmail.com>
2025-07-21 10:29:48 +0530

    First Commit

---
+++ helloworld.py
@@ -0,0 +1,21 @@
+print("This is my first step towards programming language")
+
+# data type 
+
+
+# firstName = "sunil"
+# lastName = "Kumar"
+
+# print(firstName + " " + lastName)
+
+# firstNum = "5555"
+# secondNum = "2222"
+# print(firstNum + secondNum)
+
+
+num1 = int(input("Enter 1st no : "))
+print(num1)
+num2 = int(input("enter second no : "))
+print(num2)
+
+print(num1 + num2 )
